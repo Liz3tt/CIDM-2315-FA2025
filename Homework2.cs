ae8826234c82811a01fd4971d3e122c186a02fbf
Liz3tt <lmsilva1@buffs.wtamu.edu>
2025-09-08 23:44:03 -0500

    Upload homework 2
    Part 1 complete, will need assistance with part 2...

---
+++ Homework2.cs
@@ -0,0 +1,34 @@
+﻿namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a letter grade: ");
+            string grade = Console.ReadLine().ToUpper(); // normalize input
+
+            switch (grade)
+            {
+                case "A":
+                    Console.WriteLine("Letter Grade: A");
+                    break;
+                case "B":
+                    Console.WriteLine("Letter Grade: B");
+                    break;
+                case "C":
+                    Console.WriteLine("Letter Grade: C");
+                    break;
+                case "D":
+                    Console.WriteLine("Letter Grade: D");
+                    break;
+                case "F":
+                    Console.WriteLine("Letter Grade: F");
+                    break;
+                default:
+                    Console.WriteLine("Wrong Letter Grade!");
+                    break;
+            }
+        }
+    }
+}
+
